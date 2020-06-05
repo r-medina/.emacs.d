210f4834c24787f9e57c0ab4e9cb5770b3c286f1
Ricky Medina <ricky.medina91@gmail.com>
2020-06-05 16:03:42 -0400

    themse + magit shortcut

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes (quote (bergen)))
+ '(custom-enabled-themes (quote (atom-dark)))
  '(custom-safe-themes
    (quote
     ("0a8b8aad17f3bd4af1839a49cd97048c90db98a088b1f6f6d0d2187a3c35321d" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "319bf1bab5d05e3a4c4a165efe69d27b3d975759034074f15fe61e92c7304884" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
